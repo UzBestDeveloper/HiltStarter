62c53e2b23af17a0aba99c0ffd966e6f3654c8e1
UzBestdeveloper <uzbestdeveloper@gmail.com>
2024-01-09 11:20:16 +0500

    fix

--- app/src/main/java/com/example/hiltstarter/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/hiltstarter/ui/activities/MainActivity.kt
@@ -1,20 +1,46 @@
 package com.example.hiltstarter.ui.activities
 
+import android.content.Intent
 import android.os.Bundle
 import com.example.hiltstarter.databinding.ActivityMainBinding
 import com.example.hiltstarter.ui.base.BaseActivity
+import com.example.hiltstarter.ui.fragments.home.OAuthManager
+import com.example.hiltstarter.ui.fragments.home.OpenIDConnectHelper
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity() {
 
     private lateinit var mBinding: ActivityMainBinding
+    private lateinit var oidcHelper: OpenIDConnectHelper
+    private var oAuthManager: OAuthManager? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
+
+        oAuthManager = OAuthManager(this)
+        oAuthManager!!.initiateOAuth()
     }
 
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        val callbackUri = intent.data
+        if (callbackUri != null) {
+            oAuthManager!!.handleAuthorizationCallback(callbackUri, object :
+                OAuthManager.AuthorizationCallback {
+                override fun onAuthorizationSuccess(accessToken: OAuthManager.AccessToken) {
+                    // Handle successful authorization
+                    // Save the access token for future requests
+                }
+
+                override fun onAuthorizationFailed() {
+                    // Handle failed authorization
+                }
+            })
+        }
+    }
 
 }
--- app/src/main/java/com/example/hiltstarter/ui/fragments/home/HomeFragment.kt
+++ app/src/main/java/com/example/hiltstarter/ui/fragments/home/HomeFragment.kt
@@ -1,11 +1,17 @@
 package com.example.hiltstarter.ui.fragments.home
 
+import android.content.Intent
+import android.net.Uri
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.fragment.app.viewModels
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.hiltstarter.R
 import com.example.hiltstarter.databinding.FragmentHomeBinding
 import com.example.hiltstarter.ui.base.BaseFragment
+import com.example.hiltstarter.utils.singleClickable
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class HomeFragment : BaseFragment(
@@ -15,8 +21,15 @@ class HomeFragment : BaseFragment(
     private val binding: FragmentHomeBinding by viewBinding()
     private val viewModel: HomeViewModel by viewModels()
 
-    override fun setup() {
-        viewModel.login()
+    override fun setup() = with(binding) {
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+                // do your handling codes here, which url is the requested url
+                // probably you need to open that url rather than redirect:
+                view.loadUrl(url)
+                return false // then it is not handled by default action
+            }
+        }
     }
 
     override fun observe() {
@@ -24,10 +37,19 @@ class HomeFragment : BaseFragment(
     }
 
     override fun clicks() = with(binding) {
-
+        btnAuthenticate.singleClickable {
+//            val your_client_id = "ihma_ins_web"
+//            val your_redirect_uri = "com.example.hiltstarter"
+//            val your_requested_scopes = "ihma_ins_api"
+//
+//            val authUrl = "https://tauth.ihmatest.uz/Account/Login" +
+//                    "?response_type=code" +
+//                    "&client_id=${your_client_id}" +
+//                    "&redirect_uri=${your_redirect_uri}" +
+//                    "&scope=${your_requested_scopes}"
+//            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(authUrl))
+//            startActivity(intent)
+        }
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-    }
 }
